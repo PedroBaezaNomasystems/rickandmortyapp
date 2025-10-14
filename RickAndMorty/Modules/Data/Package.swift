dc297d82228f86a77238155f24eb6fff4cfb8b02
Pedro Baeza <pedro.baeza@nomasystems.com>
2025-10-14 13:54:19 +0200

    feat: added project modules for clean architecture.

---
+++ RickAndMorty/Modules/Data/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "Data",
+    products: [
+        .library(name: "Data", targets: ["Data"])
+    ],
+    dependencies: [
+        .package(path: "../Domain")
+    ],
+    targets: [
+        .target(name: "Data", dependencies: ["Domain"]),
+        .testTarget(name: "DataTests", dependencies: ["Data"])
+    ]
+)
---
+++ RickAndMorty/Modules/Presentation/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "Presentation",
+    products: [
+        .library(name: "Presentation", targets: ["Presentation"])
+    ],
+    dependencies: [
+        .package(path: "../Domain"),
+        .package(path: "../Data")
+    ],
+    targets: [
+        .target(name: "Presentation", dependencies: ["Domain", "Data"]),
+        .testTarget(name: "PresentationTests", dependencies: ["Presentation"])
+    ]
+)
