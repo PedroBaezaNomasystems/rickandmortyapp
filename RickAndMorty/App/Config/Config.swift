59ec7fc874883710afccb3528cb98cf9d9400ddb
Pedro Baeza <pedro.baeza@nomasystems.com>
2025-10-15 12:09:34 +0200

    feat: added base url to config file.

--- RickAndMorty/App/Config/Config.swift
+++ RickAndMorty/App/Config/Config.swift
@@ -5,3 +5,20 @@
 //  Created by Pedro Juan Baeza Gómez on 15/10/25.
 //
 
+import Foundation
+
+struct Config {
+    
+    static var baseURL: URL? {
+        
+        guard let dictionary = Bundle.main.infoDictionary else {
+            return nil
+        }
+        
+        guard let baseUrlString = dictionary["BASE_URL"] as? String else {
+            return nil
+        }
+        
+        return URL(string: baseUrlString)
+    }
+}
