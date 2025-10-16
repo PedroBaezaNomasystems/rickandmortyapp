17c87dce92569a84ff83f7850ca6f278aece7a44
Pedro Baeza <pedro.baeza@nomasystems.com>
2025-10-16 14:42:30 +0200

    feat: refactor views

--- RickAndMorty/Features/Character/Detail/CharacterDetailView.swift
+++ RickAndMorty/Features/Character/Detail/CharacterDetailView.swift
@@ -9,52 +9,71 @@ import SwiftUI
 import Domain
 import Presentation
 
-struct CharacterDetailView: View {    
+struct CharacterDetailView: View {
+    
     @StateObject var viewModel: CharacterDetailViewModel
     
     init(router: Routing? = nil, characterId: String = "1") {
+        
         _viewModel = StateObject(wrappedValue: CharacterDetailViewModel(router: router, characterId: characterId))
     }
     
     var body: some View {
+        
         ZStack {
+            
             if let character = viewModel.character {
-                VStack {
-                    UrlImage(
-                        url: character.image,
-                        width: 60,
-                        height: 60
-                    )
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.gray.opacity(0.3), lineWidth: 1))
-                    
-                    VStack(alignment: .center) {
-                        Text(character.name)
-                            .font(.openSansBold(size: .title))
-                        
-                        Text("\(character.status) - \(character.species)")
-                            .font(.openSansRegular(size: .label))
-                    }
-                    Spacer()
-                }
+                
+                CharacterDetailViewItem(character: character)
             }
             
             if viewModel.isLoading {
+                
                 FullProgress()
             }
         }
         .alert(isPresented: $viewModel.isError) {
-            errorAlert
+            
+            CharacterDetailViewError
         }
         .onAppear {
+            
             Task {
+                
                 await viewModel.onAppear()
             }
         }
         .navigationTitle("character_detail_title")
     }
     
-    private var errorAlert: Alert {
+    @ViewBuilder
+    private func CharacterDetailViewItem(character: CharacterEntity) -> some View {
+        
+        VStack {
+            
+            UrlImage(
+                url: character.image,
+                width: 60,
+                height: 60
+            )
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray.opacity(0.3), lineWidth: 1))
+            
+            VStack(alignment: .center) {
+                
+                Text(character.name)
+                    .font(.openSansBold(size: .title))
+                
+                Text("\(character.status) - \(character.species)")
+                    .font(.openSansRegular(size: .label))
+            }
+            
+            Spacer()
+        }
+    }
+    
+    private var CharacterDetailViewError: Alert {
+        
         Alert(
             title: Text("character_detail_error_title"),
             message: Text("character_detail_error_message"),
@@ -64,6 +83,7 @@ struct CharacterDetailView: View {
     }
 }
 
-#Preview {    
+#Preview {
+    
     CharacterDetailView()
 }
--- RickAndMorty/Features/Character/List/CharacterListView.swift
+++ RickAndMorty/Features/Character/List/CharacterListView.swift
@@ -18,63 +18,89 @@ struct CharacterListView: View {
         _viewModel = StateObject(wrappedValue: CharacterListViewModel(router: router))
     }
     
-    var body: some View {        
+    var body: some View {
+        
         List {
+            
             ForEach(viewModel.characters, id: \.id) { character in
-                HStack {
-                    UrlImage(
-                        url: character.image,
-                        width: 60,
-                        height: 60
-                    )
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.gray.opacity(0.3), lineWidth: 1))
-                    
-                    VStack(alignment: .leading) {
-                        Text(character.name)
-                            .font(.openSansBold(size: .title))
-                        
-                        Text("\(character.status) - \(character.species)")
-                            .font(.openSansRegular(size: .label))
-                    }
-                }
-                .onTapGesture {
-                    viewModel.onClickOnCharacter(id: character.id)
-                }
-                .onAppear {
-                    if character.id == viewModel.characters.last?.id {
-                        Task {
-                            await viewModel.onRequestMoreCharacters()
-                        }
-                    }
-                }
+                
+                CharacterListViewItem(character: character)
             }
             
             if !viewModel.isLoading && !viewModel.characters.isEmpty {
+                
                 ListProgress()
             }
         }
         .refreshable {
+            
             Task {
+                
                 await viewModel.onRefresh()
             }
         }
         .overlay {
+            
             if viewModel.characters.isEmpty && !viewModel.isLoading {
-                ContentUnavailableView(
-                    "character_list_empty_list",
-                    systemImage: SystemIcon.persons.rawValue,
-                    description: Text("common_pull_to_refresh")
-                )
+                
+                CharacterListViewEmpty
             }
         }
         .alert(isPresented: $viewModel.isError) {
-            errorAlert
+            
+            CharacterListViewError
         }
         .navigationTitle("character_list_title")
     }
     
-    private var errorAlert: Alert {
+    @ViewBuilder
+    private func CharacterListViewItem(character: CharacterEntity) -> some View {
+        
+        HStack {
+            
+            UrlImage(
+                url: character.image,
+                width: 60,
+                height: 60
+            )
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray.opacity(0.3), lineWidth: 1))
+            
+            VStack(alignment: .leading) {
+                
+                Text(character.name)
+                    .font(.openSansBold(size: .title))
+                
+                Text("\(character.status) - \(character.species)")
+                    .font(.openSansRegular(size: .label))
+            }
+        }
+        .onTapGesture {
+            
+            viewModel.onClickOnCharacter(id: character.id)
+        }
+        .onAppear {
+            
+            if character.id == viewModel.characters.last?.id {
+                
+                Task {
+                    
+                    await viewModel.onRequestMoreCharacters()
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var CharacterListViewEmpty: some View {
+        ContentUnavailableView(
+            "character_list_empty_list",
+            systemImage: SystemIcon.persons.rawValue,
+            description: Text("common_pull_to_refresh")
+        )
+    }
+    
+    private var CharacterListViewError: Alert {
         
         Alert(
             title: Text("character_list_error_title"),
